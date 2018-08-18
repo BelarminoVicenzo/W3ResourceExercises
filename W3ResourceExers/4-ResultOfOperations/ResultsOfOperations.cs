dd72fa69fdc756f8d5f97a1a243887835d83b43f
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2018-08-18 17:12:59 +0100

    4 and 5

--- W3ResourceExers/4-ResultOfOperations/ResultsOfOperations.cs
+++ W3ResourceExers/4-ResultOfOperations/ResultsOfOperations.cs
@@ -8,8 +8,36 @@ namespace _4_ResultOfOperations
 {
     class ResultsOfOperations
     {
+
+        // 4. Write a C# Sharp program to print the result of the specified operations
+
+        /*
+         Test data:
+
+          -1 + 4 * 6
+          ( 35+ 5 ) % 7
+          14 + -4 * 6 / 11
+          2 + 15 / 6 * 1 - 7 % 2
+      
+
+      Expected Output:
+        23,5,12,3
+
+     */
+
+         
         static void Main(string[] args)
         {
+
+            Console.WriteLine(-1 + 4 * 6);
+            Console.WriteLine((35 + 5) % 7);
+            Console.WriteLine(14 + (-4) * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
+            
+
+
+
         }
     }
 }
---
+++ W3ResourceExers/5-SwapTwoNumbers/SwapTwoNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_SwapTwoNumbers
+{
+    class SwapTwoNumbers
+    {
+        /*
+
+       5. Write a C# Sharp program to swap two numbers.  
+         Test Data:
+         Input the First Number : 5
+         Input the Second Number : 6
+         Expected Output:
+         After Swapping :
+         First Number : 6
+         Second Number : 5 
+                  */
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            Console.WriteLine("Input the First Number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the Second Number");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("First Number: {1}\n" +
+                "Second Number: {0}", firstNumber, secondNumber);
+        }
+    }
+}
