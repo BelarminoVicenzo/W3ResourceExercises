6b2591a00ed78ec95c42fe29fb4180dce5e7ca58
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2018-08-22 21:45:32 +0100

    All set 4 tenth

--- W3ResourceExers/10-ThreeNumbersInput/ThreeNumbersInput.cs
+++ W3ResourceExers/10-ThreeNumbersInput/ThreeNumbersInput.cs
@@ -15,9 +15,9 @@ namespace _10_ThreeNumbersInput
         as input and print the output of (x+y).z and x.y + y.z
 
         Test Data:
-        Enter first number - 5
-        Enter second number - 6
-        Enter third number - 7
+        Enter first number - x
+        Enter second number - y
+        Enter third number - z
 
         Expected Output:
         Result of specified numbers 5, 6 and 7, (x+y).z is 77 and x.y + y.z is 72
@@ -29,6 +29,21 @@ namespace _10_ThreeNumbersInput
 
         static void Main(string[] args)
         {
+            double x, y, z;
+            Console.WriteLine("Write 3 numbers\n");
+
+            Console.Write("#1: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.Write("#2: ");
+            y = double.Parse(Console.ReadLine());
+
+
+            Console.Write("#3: ");
+            z = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"({x}+{y}).{z}={(x + y) * z}");
+            Console.WriteLine($"({x}.{y})+({y}.{z})= {(x * y) + (y * z)}");
         }
     }
 }
