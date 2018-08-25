8de499229eff9ee13a341bd97dec80677b721541
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2018-08-25 13:03:31 +0100

    Fix comments

--- W3ResourceExers/13-Rectangle/Rectangle.cs
+++ W3ResourceExers/13-Rectangle/Rectangle.cs
@@ -9,15 +9,17 @@ namespace _13_Rectangle
     class Rectangle
     {
 
-        //        13. Write a C# program that takes a number as input and then displays a rectangle of 3 columns wide and 5 rows tall using that digit. Go to the editor
-        //Test Data:
-        //Enter a number: 5
-        //Expected Output:
-        //555
-        //5 5
-        //5 5
-        //5 5
-        //555
+        // 13. Write a C# program that takes a number as input and 
+        // then displays a rectangle of 3 columns wide and 5 rows 
+        // tall using that digit. Go to the editor
+        // Test Data:
+        // Enter a number: 5
+        // Expected Output:
+        // 555
+        // 5 5
+        // 5 5
+        // 5 5
+        // 555
         static void Main(string[] args)
         {
         }
