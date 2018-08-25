3d2da585e9545240233ec9783c8c065805e79144
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2018-08-25 12:23:23 +0100

    All set for 11.
    It seemed to be to simple, maybe I did it wrong

--- W3ResourceExers/11-AgeULookOlder/AgeULookOlder.cs
+++ W3ResourceExers/11-AgeULookOlder/AgeULookOlder.cs
@@ -9,15 +9,22 @@ namespace _11_AgeULookOlder
     class AgeULookOlder
     {
 
-        //        11. Write a C# Sharp program that takes an age (for example 20) as input and prints something as "You look older than 20". Go to the editor
-        //Test Data:
-        //Enter your age - 25
-        //Expected Output:
-        //You look older than 25
+        // 11. Write a C# Sharp program that takes an age (for example 20) 
+        // as input and prints something as "You look older than 20". 
+        // Test Data:
+        // Enter your age - 25
+        // Expected Output:
+        // You look older than 25
 
 
         static void Main(string[] args)
         {
+            int age = 0;
+            Console.WriteLine("Enter your age");
+            age = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine($"You look older than {age}");
         }
     }
 }
