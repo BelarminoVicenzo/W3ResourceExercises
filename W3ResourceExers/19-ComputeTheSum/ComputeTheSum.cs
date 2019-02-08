56d4e2fc35893732ef3ab6a087322b739cbdb209
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2019-02-08 17:36:10 +0100

    19 all set
    19 okay

--- W3ResourceExers/19-ComputeTheSum/ComputeTheSum.cs
+++ W3ResourceExers/19-ComputeTheSum/ComputeTheSum.cs
@@ -9,9 +9,30 @@ namespace _19_ComputeTheSum
     class ComputeTheSum
     {
 
-        //19. Write a C# program to compute the sum of two given integers, if two values are equal then return the triple of their sum. Go to the editor
+        //19. Write a C# program to compute the sum of two given integers,
+        //if two values are equal then return the triple of their sum.
+        //Go to the editor
         static void Main(string[] args)
         {
+            int firstInteger = default(int);
+            int secondInteger = default(int);
+
+            Console.WriteLine("Type the first Integer");
+            firstInteger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type the second Integer");
+            secondInteger = Convert.ToInt32(Console.ReadLine());
+
+            if (firstInteger != secondInteger)
+            {
+                Console.WriteLine("Result: {0}", (secondInteger + firstInteger));
+            }
+            else
+            {
+                Console.WriteLine("Tripled result: {0}", (secondInteger + firstInteger) * 3);
+            }
+            Console.ReadLine();
         }
+
     }
 }
+
