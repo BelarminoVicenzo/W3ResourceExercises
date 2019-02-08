5fe2b9285740e685605e10fc76f0ad70b99ea911
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2019-02-08 17:48:47 +0100

    20 all set
    20 is okay

--- W3ResourceExers/20-AbsoluteValueOfDifference/AbsoluteValueOfDifference.cs
+++ W3ResourceExers/20-AbsoluteValueOfDifference/AbsoluteValueOfDifference.cs
@@ -8,9 +8,35 @@ namespace _20_AbsoluteValueOfDifference
 {
     class AbsoluteValueOfDifference
     {
-        //20. Write a C# program to get the absolute value of the difference between two given numbers. Return double the absolute value of the difference if the first number is greater than second number. Go to the editor
+        //20. Write a C# program to get the absolute value 
+        //of the difference between two given numbers. 
+        //Return double the absolute value of the difference if
+        //the first number is greater than second number. Go to the editor
+
         static void Main(string[] args)
         {
+            double firstNumber=default(double);
+            double secondNumber=default(double);
+
+            Console.WriteLine("Type the first Integer");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Type the second Integer");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            var theDiference = firstNumber - secondNumber;
+         
+
+            if (theDiference<0)
+            {
+                Console.WriteLine("The absolute value: {0}", Math.Abs(theDiference));
+            }
+            else if(firstNumber>secondNumber)
+            {
+                Console.WriteLine("Double of the absolute value: {0}", Math.Abs(theDiference) * 2);
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
--- W3ResourceExers/21_IsTwentyOrEqual/IsTwentyOrEqual.cs
+++ W3ResourceExers/21_IsTwentyOrEqual/IsTwentyOrEqual.cs
@@ -10,7 +10,9 @@ namespace _21_IsTwentyOrEqual
     {
 
 
-        //        21. Write a C# program to check the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20. Go to the editor
+        //21. Write a C# program to check the sum of the two given integers 
+        //and return true if one of the integer is 20 or if their sum is 20. 
+        //Go to the editor
         //Click me to see the solution
         static void Main(string[] args)
         {
