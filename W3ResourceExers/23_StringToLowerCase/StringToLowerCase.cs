4b2962fd5b8600ae9a8bd5f9e33e0141a2843e90
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2019-05-27 17:01:22 +0100

    23 All set

--- W3ResourceExers/23_StringToLowerCase/StringToLowerCase.cs
+++ W3ResourceExers/23_StringToLowerCase/StringToLowerCase.cs
@@ -9,12 +9,15 @@ namespace _23_StringToLowerCase
     class StringToLowerCase
     {
 
-        //        23. Write a C# program to convert a given string into lowercase. Go to the editor
+        //23. Write a C# program to convert a given string into lowercase. Go to the editor
         //Sample Output:
         //write a c# sharp program to display the following pattern using the alphabet.
         //Click me to see the solution
         static void Main(string[] args)
         {
+            string text = "Please Input a Text";
+            Console.WriteLine(text.ToLower());
+            Console.ReadLine();
         }
     }
 }
